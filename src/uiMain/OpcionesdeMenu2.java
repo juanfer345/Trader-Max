27ffd28a295219544fb85ed29d4b1078f5e94440
julian ossa <jossaz@unal.edu.co>
2019-06-19 16:33:56 -0500

    mas numeraciones de opciones de menu

---
+++ src/uiMain/OpcionesdeMenu2.java
@@ -0,0 +1,143 @@
+package uiMain;
+
+class vaciarCarrito extends OpcionDeMenu { // opcion 8
+
+	@Override
+	public void ejecutar() {
+		
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+class quitarProducto extends OpcionDeMenu { // opcion 9
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class agregarReseña extends OpcionDeMenu { // opcion 10
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class agregarComentario extends OpcionDeMenu { // opcion 11
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class subirProducto extends OpcionDeMenu { // opcion 12
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class eliminarOpcion extends OpcionDeMenu { // opcion 13
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class agregarOpcion extends OpcionDeMenu { // opcion 14
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+class eliminarCuenta extends OpcionDeMenu { // opcion 15
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+class getNumCuentas extends OpcionDeMenu { // opcion 16
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
