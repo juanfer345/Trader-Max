030b362df85da45f3f0163a4889a746e6b784920
mpgarciav <51894580+mpgarciav@users.noreply.github.com>
2019-08-25 14:27:21 -0500

    Agregar opción

--- src/control/Cuenta/Administrador/ControlAgregarOpcion.java
+++ src/control/Cuenta/Administrador/ControlAgregarOpcion.java
@@ -24,12 +24,12 @@ public class ControlAgregarOpcion implements ActionListener  {
 		
 
 			//Control de ingreso tipo de usuario
-			tipoDeCuenta = ControlErrorDatos.controlByte((byte) 1, (byte) 3, OpcionDeMenu.sb.toString(), "Por favor ingrese un número entero");
+			//tipoDeCuenta = ControlErrorDatos.controlByte((byte) 1, (byte) 3, OpcionDeMenu.sb.toString(), "Por favor ingrese un número entero");
 			if (OpcionDeMenu.controlError) {System.out.println(); return;}
 
 			while (!OpcionDeMenu.controlError) {
 				//Control de ingreso de identificación de usuario
-				idUsuario = ControlErrorDatos.controlEntero(1, Integer.MAX_VALUE, "Por favor ingrese el número identificador del usuario", "El identificador del usuario debe ser un número entero");
+				//idUsuario = ControlErrorDatos.controlEntero(1, Integer.MAX_VALUE, "Por favor ingrese el número identificador del usuario", "El identificador del usuario debe ser un número entero");
 				if (OpcionDeMenu.controlError) {System.out.println(); return;}
 
 				if (idUsuario == usuario.getId() && tipoDeCuenta == 3) {
@@ -62,7 +62,7 @@ public class ControlAgregarOpcion implements ActionListener  {
 				System.out.print(menuOpcionesDisponibles);
 
 				//Elección de la opción por parte del usuario
-				opcionUsuario = ControlErrorDatos.controlByte((byte) 1, MenuDeConsola.getsizeOpcionesComp(), "Ingrese el indice de la opción que desea agregar", "Por favor ingrese un número entero");
+	//			opcionUsuario = ControlErrorDatos.controlByte((byte) 1, MenuDeConsola.getsizeOpcionesComp(), "Ingrese el indice de la opción que desea agregar", "Por favor ingrese un número entero");
 				if (OpcionDeMenu.controlError) {System.out.println(); return;}
 
 				//Ejecución del método principal
