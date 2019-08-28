06f6f256b9eed951f9c5f37eab8fdcad9a19a041
jcardenasv <51891077+jcardenasv@users.noreply.github.com>
2019-08-28 03:30:31 -0500

    Buscar Producto :+1:

--- src/control/ControlBuscarProducto.java
+++ src/control/ControlBuscarProducto.java
@@ -3,21 +3,19 @@ package control;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-import control.Errores.ErrorTipodeDato;
+import control.Errores.ErrorAplicacion;
 import control.Errores.MetodosConError;
 import gestorAplicacion.Usuarios.Cuenta;
-import gestorAplicacion.Usuarios.Visitante;
 import uiMain.InicializacionAplicacion;
 import uiMain.MenuConsola.OpcionDeMenu;
 import uiMain.vista.PanelBuscarProducto;
 
-public class ControlBuscarProducto extends OpcionDeMenu implements ActionListener{
+public class ControlBuscarProducto extends OpcionDeMenu implements ActionListener {
 	PanelBuscarProducto buscarprod;
 	String resultado;
-	
+
 	/*
 	 * Propósito: Ejecutar el metodo buscarProducto() haciendo los respectivos
 	 * controles de error del ingreso de datos
@@ -26,48 +24,56 @@ public class ControlBuscarProducto extends OpcionDeMenu implements ActionListene
 	@Override
 	public void actionPerformed(ActionEvent arg) {
 
-		Visitante usuario = (Visitante) InicializacionAplicacion.usuarioActivo;
-		PanelBuscarProducto bp = new PanelBuscarProducto();
-
 		// Verificación de catalogo no vacío
 		if (!Cuenta.getCatalogo().isEmpty()) {
-			bp.lanzar();
-			resultado = buscarprod.texto.getText();
-			if ((arg.getSource() instanceof JButton) && !(resultado.equals(""))) {
-				// Condicional según selección
-				if (arg.getActionCommand().equals("Buscar por código")) {
+			Object[] opciones = { "Si", "No" };
 
-					try {
-						resultado = usuario.buscarProducto(Integer.parseInt(buscarprod.texto.getText()));
-					}
-					catch (NumberFormatException NF){
-						JOptionPane.showMessageDialog(null, "Debe ingresar un numero entero", "Error",
-								JOptionPane.ERROR_MESSAGE);
-					}
-					JOptionPane.showMessageDialog(null, resultado, "Informacion",
-							JOptionPane.INFORMATION_MESSAGE);
-				}else if(arg.getActionCommand().equals("Buscar por nombre")) {
-					try {
-						resultado = MetodosConError.controlString(resultado,"","Error, no ingresó un string");
-					}catch(ErrorTipodeDato e){
-						JOptionPane.showMessageDialog(null, e, "Advertencia",
-								JOptionPane.WARNING_MESSAGE);
-					}
+			int resp = JOptionPane.showOptionDialog(null, "Buscar el producto por código", "Buscar producto",
+					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
 
+			if (JOptionPane.OK_OPTION == resp) {
+				String cod = JOptionPane.showInputDialog(null, "Código del producto");
+				if (cod != null) {
+					try {
+						int codigo = MetodosConError.controlNumero((cod), 1, Integer.MAX_VALUE, "Código",
+								"Por favor ingrese un codigo válido");
+						JOptionPane.showMessageDialog(null,
+								InicializacionAplicacion.usuarioActivo.buscarProducto(codigo), "Notificación",
+								JOptionPane.INFORMATION_MESSAGE);
+					} catch (ErrorAplicacion e1) {
+						JOptionPane.showMessageDialog(null, "Manejo de errores de la Aplicación: " + e1.getMessage(),
+								"Error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				} else {
+					return;
+				}
+			} else {
+				Object[] opciones2 = { "Si", "No" };
+
+				int resp2 = JOptionPane.showOptionDialog(null, "Buscar el producto por nombre", "Buscar Producto",
+						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones2, opciones[0]);
+
+				if (JOptionPane.OK_OPTION == resp2) {
+					String cod = JOptionPane.showInputDialog(null, "Nombre del producto");
+					if (cod != null) {
+						try {
+							String codigo = MetodosConError.controlString((cod), "Nombre",
+									"Por favor ingrese un nombre válido");
+							JOptionPane.showMessageDialog(null,
+									InicializacionAplicacion.usuarioActivo.buscarProducto(codigo), "Notificación",
+									JOptionPane.INFORMATION_MESSAGE);
+						} catch (ErrorAplicacion e1) {
+							JOptionPane.showMessageDialog(null,
+									"Manejo de errores de la Aplicación: " + e1.getMessage(), "Error",
+									JOptionPane.ERROR_MESSAGE);
+							return;
+						}
+					} else {
+						return;
+					}
 				}
 			}
-			else {
-				if(resultado.equals("")) {
-					JOptionPane.showMessageDialog(null, "Debe ingresar algo en el campo de texto", "Advertencia",
-							JOptionPane.WARNING_MESSAGE);
-				}
-			}
-
-		}
-		else {
-			JOptionPane.showMessageDialog(null, "El catálogo se encuentra vacío", "Informacion",
-					JOptionPane.ERROR_MESSAGE);
-
 		}
 	}
 
