91a46bd668f67b0eb99a9a92f422d84869b94d4a
jcardenasv <51891077+jcardenasv@users.noreply.github.com>
2019-08-28 00:56:05 -0500

    Arreglos :coffee:

--- src/control/Cuenta/Comprador/ControlComprarProductos.java
+++ src/control/Cuenta/Comprador/ControlComprarProductos.java
@@ -13,20 +13,17 @@ import uiMain.MenuConsola.OpcionDeMenu;
 public class ControlComprarProductos extends OpcionDeMenu implements ActionListener {
 
 	public void actionPerformed(ActionEvent arg) {
-		byte seleccion=0;
+
 		OpcionDeMenu.controlError = false;
 		switch (((String) arg.getActionCommand())) {
 		case "Comprar productos":
 			if (!CarritoDeCompras.getProductos().isEmpty()) {
-				// Ingreso del dato por parte del usuario
-				//seleccion = ErrorAplicacion.controlByte((byte) 1, (byte) 2, sb.toString(), "El dato que ingresó es inválido, vuelva a intentarlo");
-				//if (OpcionDeMenu.controlError || seleccion == 2) {return;}
-
-				//Ejecución del método
-				JOptionPane.showMessageDialog(null, CarritoDeCompras.comprarProductos(), "Comprar Productos", JOptionPane.INFORMATION_MESSAGE);
-			}
-			else {
-				JOptionPane.showMessageDialog(null, "Su carrito de compras esta vacío, no hay ningún producto pendiente por comprar.", "Comprar Productos", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null, CarritoDeCompras.comprarProductos(), "Comprar Productos",
+						JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Su carrito de compras esta vacío, no hay ningún producto pendiente por comprar.",
+						"Comprar Productos", JOptionPane.WARNING_MESSAGE);
 			}
 		}
 	}
